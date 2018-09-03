8e8833fa3bbeec0b3e35d984cce96461f7f95e67
James McDonald <james@jamesmcdonald.com>
2018-09-03 09:30:07 +0200

    Initial Commit

---
+++ Passmenu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Passmenu
+//
+//  Created by James McDonald on 03/09/2018.
+//  Copyright © 2018 James McDonald. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Passmenu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Passmenu
+//
+//  Created by James McDonald on 03/09/2018.
+//  Copyright © 2018 James McDonald. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
