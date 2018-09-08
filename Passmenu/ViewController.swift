1049fc844b5b7e477308fee15290ba38f618aacd
James McDonald <james@jamesmcdonald.com>
2018-09-08 18:18:39 +0200

    Add notification (and rearrange source a little)

--- Passmenu/ViewController.swift
+++ Passmenu/ViewController.swift
@@ -13,35 +13,12 @@ let clearInterval = 45
 
 class ViewController: NSViewController {
     var timer:Timer? = nil
-
-    @IBOutlet weak var passwordField: NSTextField!
-    @IBOutlet weak var resultTable: NSTableView!
-    @IBAction func doubleClick(_ sender: NSTableView) {
-        if sender.selectedRow < 0 {
-            return
-        }
-        // Hide the window before starting or pinentry feels clumsy
-        let delegate = NSApp.delegate! as! AppDelegate
-        delegate.hideSearch(nil)
-        
-        let entry = results[sender.selectedRow]
-        let pw = pass.getPass(entry)
-
-        // Cancel pending timer
-        if timer != nil {
-            timer!.invalidate()
-        }
-        let pasteboard = NSPasteboard.general
-        pasteboard.clearContents()
-        pasteboard.setString(pw, forType: .string)
-        timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(clearInterval), repeats: false)  { (timer) in
-            pasteboard.clearContents()
-        }
-    }
-    
     let pass = PassFacade()
     var results: [String] = []
 
+    @IBOutlet weak var passwordField: NSTextField!
+    @IBOutlet weak var resultTable: NSTableView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         self.resultTable.delegate = self
@@ -68,6 +45,7 @@ class ViewController: NSViewController {
             }
         }
     }
+    
     override func viewWillAppear() {
         super.viewWillAppear()
         self.view.window?.titleVisibility = .hidden
@@ -81,6 +59,7 @@ class ViewController: NSViewController {
         self.view.window?.styleMask.remove(.resizable)
 
     }
+    
     override func viewDidAppear() {
         super.viewDidAppear()
         passwordField.selectText(self)
@@ -102,7 +81,38 @@ class ViewController: NSViewController {
             resultTable.selectRowIndexes([0], byExtendingSelection: false)
         }
     }
+    
+    func notify(_ pass: String) {
+        let notification = NSUserNotification()
+        notification.title = "Password Copied"
+        notification.informativeText = "Password entry \"\(pass)\" copied to clipboard. It will be cleared in \(clearInterval) seconds."
+        //notification.soundName  = NSUserNotificationDefaultSoundName
+        NSUserNotificationCenter.default.deliver(notification)
+    }
 
+    @IBAction func doubleClick(_ sender: NSTableView) {
+        if sender.selectedRow < 0 {
+            return
+        }
+        // Hide the window before starting or pinentry feels clumsy
+        let delegate = NSApp.delegate! as! AppDelegate
+        delegate.hideSearch(nil)
+        
+        let entry = results[sender.selectedRow]
+        let pw = pass.getPass(entry)
+        
+        // Cancel pending timer
+        if timer != nil {
+            timer!.invalidate()
+        }
+        let pasteboard = NSPasteboard.general
+        pasteboard.clearContents()
+        pasteboard.setString(pw, forType: .string)
+        timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(clearInterval), repeats: false)  { (timer) in
+            pasteboard.clearContents()
+        }
+        notify(results[sender.selectedRow])
+    }
 }
 
 extension ViewController : NSTableViewDataSource, NSTableViewDelegate {
